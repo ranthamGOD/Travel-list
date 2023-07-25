4a210deb722a09354d1f12eeec1b4aefbfd06be5
Akash patil <ap4180530@gmail.com>
2023-07-25 14:18:26 +0530

    making changes on stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,15 @@
 export default function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
