73d3c934d9a5511860d8d14cecf6347cec31779a
Akash patil <ap4180530@gmail.com>
2023-07-25 14:23:43 +0530

    making changes on stats

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,6 @@ import Logo from "./logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "charger", quantity: 1, packed: false },
-// ];
 
 export default function App() {
   const [items, setItems] = useState([]);
